29b1ff3b6ff5def2c228824cbc3bbd25dcb3a92a
tithauliya <tithauliya21@gmail.com>
2025-11-13 15:19:55 +0700

    Add files via upload

---
+++ PERTEMUAN13/build/generated-sources/ap-source-output/pertemuan13/Akun_.java
@@ -0,0 +1,14 @@
+package pertemuan13;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-11-13T11:13:28", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Akun.class)
+public class Akun_ { 
+
+    public static volatile SingularAttribute<Akun, String> password;
+    public static volatile SingularAttribute<Akun, String> username;
+
+}
